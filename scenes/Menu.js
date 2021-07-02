2b5eb3c13b58cf8aaa00402e5de46f2960c2ce54
SeanJTran <sejtran@ucsc.edu>
2021-07-01 22:31:24 -0700

    edited menu scene

--- scenes/Menu.js
+++ scenes/Menu.js
@@ -8,11 +8,13 @@ class Menu extends Phaser.Scene{
     }
 
     create(){
-      this.add.text(game.config.width/2, game.config.height/2, "YooHoo");
-      this.scene.start('playScene');
+      this.add.text(game.config.width/2, game.config.height/2, "Menu");
+      keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
 
     update(){
-
+      if (Phaser.Input.Keyboard.JustDown(keySpace)) {
+        this.scene.start("playScene");
+      }
     }
 }
