06ae640dc027bb4e334b2d5e5735110bf2c446ff
TheUnaverageJoe <jobrcart@ucsc.edu>
2021-07-02 22:09:22 -0700

    Added initial player

--- scenes/Menu.js
+++ scenes/Menu.js
@@ -7,7 +7,23 @@ class Menu extends Phaser.Scene{
     }
 
     create(){
-      this.add.text(game.config.width/2, game.config.height/2, "Menu");
+      let menuTextConfig = {
+        fontFamily: 'Courier',
+        fontSize: '60px',
+        backgroundColor: '#FFF', //'#000000',//#F3B141
+        color: '#000',
+        align: 'center',
+        fontStyle: 'bold',
+        strokeThickness: 0,
+        stroke: '#000',
+        padding: {
+          top: 5,
+          bottom: 5,
+        },
+        fixedWidth: 0
+    }
+
+      this.add.text(game.config.width/2, game.config.height/2, "Press Space to start", menuTextConfig).setOrigin(0.5);
       keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
 
--- scenes/Play.js
+++ scenes/Play.js
@@ -1,9 +1,7 @@
 class Play extends Phaser.Scene {
     constructor() {
         super("playScene");
-        //let bg;
-        let gameStart = false;
-        //let character;
+        this.gameStart = false;
     }
     
     preload() {
@@ -11,9 +9,11 @@ class Play extends Phaser.Scene {
         this.load.image('background', './assets/background.png');
         this.load.image('character', './assets/character.png');
         this.load.image('table1', './assets/table.png');
+        this.load.image('floor', './assets/invisibleFloor.png');
     }
 
     create() {
+        this.floorSize = 10;
         let playTextConfig = {
             fontSize: '32px',
             backgroundColor: '#ffffff',
@@ -27,27 +27,51 @@ class Play extends Phaser.Scene {
         //add background tilesprites
         this.bg = this.add.tileSprite(0, 0, 720 , 480, 'background').setOrigin(0, 0);
 
+        //invisible floor object
+        this.floor = this.add.sprite(0, game.config.height - this.floorSize, 'floor');
+        this.physics.add.existing(this.floor);
+        this.floor.body.setAllowGravity(false);
+        this.floor.body.setImmovable(true);
+
         //Create Character sprite
-        this.character = new Character(this, borderUISize + borderPadding, game.config.height - borderPadding - borderUISize, 'character');
+        this.character = this.add.sprite(borderUISize + borderPadding, game.config.height - borderPadding*2 - borderUISize, 'character');
         this.physics.add.existing(this.character);
-        this.character.body.setAllowGravity(false);
-        //this.physics.disableUpdate();
+        this.physics.add.collider(this.floor, this.character);
+        this.character.body.pushable = false;
 
         //list of obsticles
         this.textureList = ['table1'];
+
+        this.physics.disableUpdate();
     }
 
     update(){
-        if(Phaser.Input.Keyboard.JustDown(keySpace) && !this.gameStart){
+        this.spaceDown = Phaser.Input.Keyboard.JustDown(keySpace)
+        if(this.spaceDown && !this.gameStart){
             this.gameStart = true;
-            //this.physics.enableUpdate();
+            this.physics.enableUpdate();
             this.spawnObsticle(this.textureList[0]);
+            console.log("preGame");
         }
 
         if(this.gameStart){
             //scroll background tile
             this.bg.tilePositionX += 1;
-            
+
+            if(this.spaceDown){
+                this.character.body.setVelocity(0, -700);
+            }
+        }
+    }
+
+    checkCollision(sprite1, sprite2) {
+        if(sprite1.x < sprite2.x + sprite2.width &&
+            sprite1.x + sprite1.width > sprite2.x &&
+            sprite1.y < sprite2.y + sprite2.height &&
+            sprite1.height + sprite1.y > sprite2.y) {
+               return true;
+        } else {
+            return false;
         }
     }
 
@@ -57,5 +81,6 @@ class Play extends Phaser.Scene {
         this.physics.add.existing(this.obsticle);
         this.obsticle.body.setAllowGravity(false);
         this.obsticle.body.setVelocity(-100, 0);
+        this.physics.add.collider(this.character, this.obsticle);
     }
 }
